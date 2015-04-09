bf667cbe18faf8a7460f00cb1e1532232488625d
AndyNhi <nhi.atn@gmail.com>
2015-04-09 12:30:51 -0600

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,14 @@
 Event.destroy_all
 
-
 15.times do
   Event.create!(
-    title: "the title",
-    description: "this is a description",
-    contact: "something@email.com",
-    address: "18th street",
-    facebook_url: "www.fb.com",
-    other_url: "somerandomurl",
-    location: "over there",
+    title: "the stussy event",
+    description: "this is a description of an awesome event happening soon",
+    contact: "stussy@email.com",
+    address: "18th street, broomfield, CO",
+    facebook_url: "www.facebook.com",
+    other_url: "www.randomlink.com",
+    location: "shopping center",
     date: DateTime.new,
     avatar: File.open(File.join(Rails.root, 'app/assets/images/stussy.jpeg'))
   )
