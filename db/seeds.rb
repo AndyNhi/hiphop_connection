18a86ace14e9fb2c9ed0ac4cd090ebb5448090d5
AndyNhi <nhi.atn@gmail.com>
2015-04-09 01:50:45 -0600

    style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
-Event.create!(
-  title: "the title",
-  description: "this is a description",
-  contact: "something@email.com",
-  address: "18th street",
-  facebook_url: "www.fb.com",
-  other_url: "somerandomurl",
-  location: "over there",
-  avatar: File.open(File.join(Rails.root, 'app/assets/images/stussy.jpeg'))
-)
+Event.destroy_all
+
+
+15.times do
+  Event.create!(
+    title: "the title",
+    description: "this is a description",
+    contact: "something@email.com",
+    address: "18th street",
+    facebook_url: "www.fb.com",
+    other_url: "somerandomurl",
+    location: "over there",
+    date: DateTime.new,
+    avatar: File.open(File.join(Rails.root, 'app/assets/images/stussy.jpeg'))
+  )
+end
