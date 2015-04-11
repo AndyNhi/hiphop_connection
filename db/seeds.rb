8c48b25165dee50d7bcae7f5b253fbb3ccf21128
AndyNhi <nhi.atn@gmail.com>
2015-04-10 18:44:19 -0600

    css update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Event.destroy_all
 
-15.times do
+2.times do
   Event.create!(
     title: "the stussy event",
     email: "admin@email.com",
@@ -10,7 +10,38 @@ Event.destroy_all
     facebook_url: "www.facebook.com",
     other_url: "www.randomlink.com",
     location: "shopping center",
-    date: DateTime.new,
+    date: 2.days.ago,
+    avatar: File.open(File.join(Rails.root, 'app/assets/images/stussy.jpeg'))
+  )
+end
+
+
+2.times do
+  Event.create!(
+    title: "secondary event",
+    email: "admin@email.com",
+    description: "this is a description of an awesome event happening soon",
+    contact: "stussy@email.com",
+    address: "18th street, broomfield, CO",
+    facebook_url: "www.facebook.com",
+    other_url: "www.randomlink.com",
+    location: "shopping center",
+    date: 6.days.from_now,
+    avatar: File.open(File.join(Rails.root, 'app/assets/images/stussy.jpeg'))
+  )
+end
+
+4.times do
+  Event.create!(
+    title: "secondary event",
+    email: "admin@email.com",
+    description: "this is a description of an awesome event happening soon",
+    contact: "stussy@email.com",
+    address: "18th street, broomfield, CO",
+    facebook_url: "www.facebook.com",
+    other_url: "www.randomlink.com",
+    location: "shopping center",
+    date: 10.days.from_now,
     avatar: File.open(File.join(Rails.root, 'app/assets/images/stussy.jpeg'))
   )
 end
